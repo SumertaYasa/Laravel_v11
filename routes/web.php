d3ac81f7164c84fa9eb81b284bb8b0e5839cc478
Yasaa <114787639+SumertaYasa@users.noreply.github.com>
2024-11-26 18:14:42 +0800

    tambah view data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,11 +8,54 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Yasa'], ['title' => 'About']);
+    return view('about', ['nama' => 'Yasa', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'judul artikel 1',
+            'author' => 'Yasa',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum odio eligendi a maxime harum! Odio, placeat sequi? 
+                    Eveniet aspernatur reprehenderit, quas iusto explicabo commodi itaque accusantium magnam, debitis, enim quo?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'judul artikel 2',
+            'author' => 'Yasa',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum odio eligendi a maxime harum! Odio, placeat sequi? 
+                    Eveniet aspernatur reprehenderit, quas iusto explicabo commodi itaque accusantium magnam, debitis, enim quo?'
+        ]
+        ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'judul artikel 1',
+            'author' => 'Yasa',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum odio eligendi a maxime harum! Odio, placeat sequi? 
+                    Eveniet aspernatur reprehenderit, quas iusto explicabo commodi itaque accusantium magnam, debitis, enim quo?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'judul artikel 2',
+            'author' => 'Yasa',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum odio eligendi a maxime harum! Odio, placeat sequi? 
+                    Eveniet aspernatur reprehenderit, quas iusto explicabo commodi itaque accusantium magnam, debitis, enim quo?'
+        ]
+    ];
+    
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
